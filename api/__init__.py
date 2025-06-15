fcd562dd31bfedafa2608090c289ccddead4156b
Chintana Meegamarachchi <chintana@appsolve.com.au>
2025-06-15 02:15:30 +0000

    Add logging configuration and HTTP exception handler

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import JSONResponse
+from api.common.logger_config import logger
 from api.features.customers.routes import customer_router
 from api.features.countries.routes import country_router
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Custom handler to log all HTTPException
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request: Request, exc: HTTPException):
+    logger.error(f"HTTPException: {exc.detail} (status_code={exc.status_code}) on path {request.url}")
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.detail},
+    )
+
+logger.info("Starting API")
 
 origins = [
     "*"
